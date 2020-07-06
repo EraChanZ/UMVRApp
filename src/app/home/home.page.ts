74b39310bcd1ca123f34c3227ca3214a94944e75
unknown <vovasachkov2003@mail.ru>
2020-07-06 17:57:06 +0300

    first commit

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -6,7 +6,11 @@ import { Component } from '@angular/core';
   styleUrls: ['home.page.scss'],
 })
 export class HomePage {
-
   constructor() {}
+  texts = ["the first thing", "the second thing"]
+  text = this.texts[0]
+  onChangeText() {
+    this.text = (this.text == this.texts[0]) ? this.texts[1] : this.texts[0]
+  }
 
 }
